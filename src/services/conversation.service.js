f8df2e98ef4c62fafaf8d79159660ac6719f644c
mayalmog <mayalmog@gmail.com>
2022-08-25 22:14:07 +0300

    favicon, breakdown of main func to separate funcs

--- src/services/conversation.service.js
+++ src/services/conversation.service.js
@@ -9,17 +9,30 @@ export const conversationService = {
 function conversationByChatId(chatID) {
   let chat = chats.filter((chat) => chat.chatID === chatID)[0];
   if (chat.questions) {
-    chat.questions.map((question) => {
-      const extendedQuestion = questions.filter(
-        (q) => q.qid === question.qid
-      )[0];
-      question["qtype"] = extendedQuestion.qtype;
-      question["qtext"] = extendedQuestion.qtext;
-      question["answers"] = answers.filter((answer) =>
-        answer.qids.includes(question.qid)
-      );
-    });
+    chat = getFormattedQuestions(chat);
   }
   console.log(chat);
   return chat;
+}
+
+function getFormattedQuestions(chat) {
+  chat.questions.map((question) => {
+    const extendedQuestion = questions.filter((q) => q.qid === question.qid)[0];
+    question["qtype"] = extendedQuestion.qtype;
+    question["qtext"] = extendedQuestion.qtext;
+    question["answers"] = getFormattedAnswers(question.qid);
+  });
+  return chat;
+}
+
+function getFormattedAnswers(qid) {
+  let formattedAnswers = [];
+  answers.map((answer) => {
+    if (answer.qids.includes(qid)) {
+      let formattedAnswer = Object.assign({}, answer);
+      delete formattedAnswer.qids;
+      formattedAnswers.push(formattedAnswer);
+    }
+  });
+  return formattedAnswers;
 }
